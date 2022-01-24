702f7b057289dab396d873112b962b66505e844c
okyaneka <okyaneka@gmail.com>
2022-01-24 17:41:54 +0700

    Cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,11 +16,8 @@ class CorsMiddleware
   public function handle($request, Closure $next)
   {
     $headers = [
-      'Access-Control-Allow-Origin'      => '*',
-      'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-      'Access-Control-Allow-Credentials' => 'true',
-      'Access-Control-Max-Age'           => '86400',
-      'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Headers' => '*'
     ];
 
     if ($request->isMethod('OPTIONS')) {
