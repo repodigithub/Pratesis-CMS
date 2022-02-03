5d4016d920f0c8a73277f85e546938f9e61ee9b4
fajar ilham rosi <fajarilhamrosi@gmail.com>
2022-02-03 13:50:44 +0700

    Create AreaFactory.php

---
+++ database/factories/AreaFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Area;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AreaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Area::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'kode_area' => $this->faker->numerify('TEST##'),
+            'nama_area' => $this->faker->name,
+            'alamat_depo' => $this->faker->name,
+            'kode_region' => 'TEST01',
+        ];
+    }
+}
