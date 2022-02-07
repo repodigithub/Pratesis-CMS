6cae2166bafa7b7318973f0fed671527ee92d73b
okyaneka <okyaneka@gmail.com>
2022-02-07 15:07:37 +0700

    Update region

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -2,144 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Imports\MasterDataImport;
+use App\Http\Controllers\Master\MasterDataController;
 use App\Models\Region;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Maatwebsite\Excel\Facades\Excel;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
-class RegionController extends Controller
+class RegionController extends MasterDataController
 {
   public function __construct()
   {
     $this->middleware("auth:api");
+    $this->model = Region::class;
+    $this->model_key = "kode_region";
   }
 
-  public function index(Request $req)
+  public function onFilter(Builder $query, Request $req)
   {
-    $pagination = $this->getPagination($req);
-
-    $data = Region::select("*");
-
     if ($req->filled("search")) {
-      $data->where(function ($query) use ($req) {
-        $query->where("kode_region", "ILIKE", "%{$req->query("search")}%");
-        $query->orWhere("nama_region", "ILIKE", "%{$req->query("search")}%");
+      $query->where(function ($q) use ($req) {
+        $q->where("kode_region", "ILIKE", "%{$req->query("search")}%");
+        $q->orWhere("nama_region", "ILIKE", "%{$req->query("search")}%");
       });
     }
+    return $query;
+  }
 
-    if (!empty($pagination->sort)) {
-      $sort = $pagination->sort;
-      $data->orderBy($sort[0], $sort[1]);
+  public function rules($data = null)
+  {
+    $rules = [];
+    if (!empty($data->id) || !empty($data = $this->model::where("kode_region", $data)->first())) {
+      $rules["kode_region"] = "required|unique:region,kode_region,$data->id";
+    } else {
+      $rules["kode_region"] = "required|unique:region";
     }
-
-    $data = $data->paginate($pagination->limit, ["*"], "page", $pagination->page);
-
-    return $this->response($data);
-  }
-
-  public function create(Request $req)
-  {
-    $this->validate($req, [
-      "kode_region" => "required|unique:region",
-      "nama_region" => "required",
-    ]);
-
-    $data = Region::create($req->all());
-
-    return $this->response($data);
-  }
-
-  public function upload(Request $req)
-  {
-    $this->validate($req, [
-      "file" => "required|file|mimes:xlsx"
-    ]);
-
-    $data = DB::transaction(function () use ($req) {
-      $file = $req->file('file');
-      $file_data = $this->storeFile(Region::class, $file);
-
-      $import = new MasterDataImport();
-      $import->onlySheets(Region::WORKSHEET_NAME);
-
-      $imported_data = Excel::toCollection($import, $file)[Region::WORKSHEET_NAME];
-
-      $data = $imported_data[0];
-      foreach (Region::FIELD_NAME as $key => $field) {
-        if ($data[$key] != $field) {
-          throw new BadRequestHttpException("#{$key}_column_error");
-        }
-      }
-
-      $imported_data->splice(0, 1);
-      $error = true;
-      foreach ($imported_data as $key => $value) {
-        if ($value[0] == "//END") {
-          $imported_data->splice($key);
-          $error = false;
-          break;
-        }
-      }
-
-      if ($error) {
-        throw new BadRequestHttpException("no_end_tag_error");
-      }
-
-      $imported_data = $imported_data->map(function ($row) {
-        return [
-          "kode_region" => $row[0],
-          "nama_region" => $row[1],
-        ];
-      });
-
-      $data = 0;
-      foreach ($imported_data as $key => $value) {
-        $this->validate(new Request($value), [
-          "kode_region" => "required",
-          "nama_region" => "required",
-        ], [
-          "required" => "The :attribute #" . ($key + 1) . " field is required",
-          "unique" => "The :attribute #" . ($key + 1) . " with value \":input\" has already been taken.",
-        ]);
-
-        Region::updateOrCreate([
-          "kode_region" => $value['kode_region'],
-        ], $value);
-        $data++;
-      }
-      $file->move(storage_path($file_data->storage_path), $file_data->filename . '.' . $file_data->type);
-      return $data;
-    });
-
-    return $this->response($data);
-  }
-
-  public function show($id, Request $req)
-  {
-    $data = $this->getModel(Region::class, $id);
-    return $this->response($data);
-  }
-
-  public function update($id, Request $req)
-  {
-    $data = $this->getModel(Region::class, $id);
-
-    $this->validate($req, [
-      "kode_region" => "required|unique:region,kode_region,$data->id",
-      "nama_region" => "required",
-    ]);
-
-    $data->update($req->all());
-
-    return $this->response($data);
-  }
-
-  public function delete($id, Request $req)
-  {
-    $data = $this->getModel(Region::class, $id);
-    $data->delete();
-    return $this->response();
+    $rules["nama_region"] = "required";
+    return $rules;
   }
 }
