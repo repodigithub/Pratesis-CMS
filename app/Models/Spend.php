198b4ff6a6377625027a626d9fada9152f781119
Oky Aneka <okyaneka@gmail.com>
2022-07-05 18:37:53 +0700

    adjust append nama kegiatan on spend

--- app/Models/Spend.php
+++ app/Models/Spend.php
@@ -11,14 +11,30 @@ class Spend extends Model
   const WORKSHEET_NAME = "SPENDTYPEMST";
   const FIELD_NAME = ["Kode Spend Type", "Kode Investment Type", "Fund Type", "Reference Pajak (CBT)", "Condition Type"];
 
-  protected $hidden = ["pivot"];
-
   protected $table = "spend_type";
 
   public $fillable = ["kode_spend_type", "kode_investment", "fund_type", "reference_tax", "condition_type"];
 
+  protected $appends = ["kegiatan"];
+
+  protected $hidden = ["pivot"];
+
   public function investment()
   {
     return $this->belongsTo(Investment::class, 'kode_investment', 'kode_investment');
+  }
+
+  public function promoType()
+  {
+    return $this->belongsToMany(TipePromo::class, "tipe_promo_spend_type",  "kode_spend_type", "kode_kegiatan", "kode_spend_type", "kode_kegiatan");
+  }
+
+  public function getKegiatanAttribute()
+  {
+    try {
+      return $this->promoType()->first()->only(['id', 'nama_kegiatan']);
+    } catch (\Throwable $th) {
+      return null;
+    }
   }
 }
