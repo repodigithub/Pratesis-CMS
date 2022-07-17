49b605ddd715c8020bc7271d55a825d4532a8085
Oky Aneka <okyaneka@gmail.com>
2022-07-17 11:14:40 +0700

    feature laporan klaim by ga

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Claim;
 use App\Models\Promo\Promo;
+use App\Models\Promo\PromoDistributor;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -80,6 +82,55 @@ class ReportController extends Controller
             $val->promo_area = $val->promoAreas()->get()->pluck('kode_area');
 
             return $val->makeHidden(['document', 'thumbnail']);
+        }));
+
+        return $this->response($data);
+    }
+
+    public function claim(Request $req)
+    {
+        if (empty(auth()->user()->kode_area)) {
+            throw new BadRequestHttpException('user_kode_area_empty');
+        }
+
+        $pagination = $this->getPagination($req);
+
+        $data = Claim::where('status', Claim::STATUS_APPROVE)
+            ->whereHas('promoDistributor', function ($q) use ($req) {
+                $q->whereIn("kode_distributor", auth()->user()->area->distributors->pluck('kode_distributor'));
+                if ($req->filled('kode_distributor')) {
+                    $q->where('kode_distributor', $req->query('kode_distributor'));
+                }
+            });
+
+
+        if ($req->filled('status_claim')) {
+            switch ($req->query('status_claim')) {
+                case Claim::LAPORAN_CAN_PAY:
+                    $data->whereNull('bukti_bayar');
+                    break;
+                case Claim::LAPORAN_PAYED:
+                    $data->whereNotNull('bukti_bayar');
+                    break;
+                case 'all':
+                default:
+                    # code...
+                    break;
+            }
+            # code...
+        }
+
+        $data = $data->paginate($pagination->limit, ["*"], "page", $pagination->page);
+        $data->setCollection($data->getCollection()->map(function ($val) {
+            $promoDistributor = $val->promoDistributor()->first();
+            $val->claim_amount = $promoDistributor->budget;
+            $tipe_promo = $promoDistributor->promo->promoType->first();
+
+            $val->ppn_percentage = number_format($tipe_promo->persentase_ppn);
+            $val->ppn_amount = number_format($val->amount * $tipe_promo->persentase_ppn / 100, 2);
+            $val->pph_percentage = number_format($tipe_promo->persentase_pph);
+            $val->pph_amount = number_format($val->amount * $tipe_promo->persentase_pph / 100, 2);
+            return $val->makeVisible('status_claim');
         }));
 
         return $this->response($data);
--- routes/api/v1/reports.php
+++ routes/api/v1/reports.php
@@ -1,11 +1,6 @@
 <?php
 
 $router->group(['prefix' => 'report'], function () use ($router) {
-  $router->group(['prefix' => 'ho'], function () use ($router) {
-    $router->get('/promo', ['uses' => 'ReportController@promo']);
-
-    $router->post('/promo', ['uses' => 'ReportController@getReportPromoByHo']);
-    $router->post('/area', ['uses' => 'ReportController@getReportAreaByHo']);
-    $router->post('/brand', ['uses' => 'ReportController@getReportBrandByHo']);
-  });
+  $router->get('/ho/promo', ['uses' => 'ReportController@promo']);
+  $router->get('/ga/claim', ['uses' => 'ReportController@claim']);
 });
